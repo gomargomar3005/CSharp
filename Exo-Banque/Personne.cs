6382bc050dfb83a67607ccc887186ebec996b842
Samuel Legrain <samuel.legrain@bstorm.be>
2023-11-29 15:00:38 +0100

    Ajout Exo p33 part1

---
+++ Exo-Banque/Personne.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo_Banque
+{
+    internal class Personne
+    {
+        /* Equivalant à l'auto-propriété
+        private string _nom;
+
+        public string Nom { 
+            get { return _nom; } 
+            set { _nom = value; } 
+        }
+        */
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+
+        public DateTime DateNaiss { get; set; }
+    }
+}
