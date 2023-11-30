b9a8a139762c36bd49d284ea2080c666ef459032
Samuel Legrain <samuel.legrain@bstorm.be>
2023-11-30 09:29:40 +0100

    Update Exo p33 Program.Main

--- Exo-Banque/Program.cs
+++ Exo-Banque/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Personne p1 = new Personne();
+            p1.Nom = "Legrain";
+            p1.Prenom = "Samuel";
+            p1.DateNaiss = new DateTime(1987, 9, 27);
+
+            Courant compte1 = new Courant();
+            compte1.Numero = "BE55 1234 1234 1234";
+            compte1.LigneDeCredit = 100;
+            compte1.Titulaire = p1;
+
+            Console.WriteLine($"Votre solde est de : {compte1.Solde} €");
+            compte1.Retrait(50);
+            Console.WriteLine($"Votre solde est de : {compte1.Solde} €");
+            compte1.Depot(1_000_000);
+            Console.WriteLine($"Votre solde est de : {compte1.Solde} €");
         }
     }
 }
