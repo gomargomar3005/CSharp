f11c079570bc44f66c4ab75a67abda312b0ae107
Samuel Legrain <samuel.legrain@bstorm.be>
2023-12-11 11:32:21 +0100

    Ajout Démo IDisposable (plus de destructeur)

---
+++ Demo-IDisposable/Formation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_IDisposable
+{
+    internal class Formation : IDisposable
+    {
+        public string Nom { get; private set; }
+        private List<string> _eleves;
+
+        public Formation(string nom,params string[]eleves)
+        {
+            Nom = nom;
+            _eleves = new List<string>(eleves);
+        }
+
+        public void Listing()
+        {
+            foreach (string name in _eleves)
+            {
+                Console.WriteLine(name);
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"La classe des {this.Nom} est composé de {_eleves.Count} élève{((_eleves.Count > 1) ? "s" : "")}";
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine(this);
+        }
+
+        //Destructeur : Aux oubliettes! On préfère l'interface IDisposable
+        ~Formation()
+        {
+            Console.WriteLine(this);
+        }
+    }
+}
---
+++ Demo-IDisposable/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Demo_IDisposable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Formation WAD23 = new Formation("WAD23",
+                "Hanane",
+                "Emanuela",
+                "Elena",
+                "Najoua",
+                "Laurence",
+                "Dorine",
+                "Hélène",
+                "Yusrâ",
+                "Hsin-I"
+                ))
+            {
+                WAD23.Listing();
+            }
+        }
+    }
+}
