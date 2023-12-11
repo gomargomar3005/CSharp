416eba90cb48e90659945d4d780d67a9a0e03778
Samuel Legrain <samuel.legrain@bstorm.be>
2023-12-11 09:56:08 +0100

    Ajout Démo Constructeur

---
+++ Demo-Constructeur/Etudiant.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Constructeur
+{
+    internal class Etudiant
+    {
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+
+        private List<int> _notes;
+        public int[] Notes
+        {
+            get { return _notes.ToArray(); }
+        }
+
+        public double Moyenne
+        {
+            get
+            {
+                double result = 0;
+                foreach (int note in _notes)
+                {
+                    result += note;
+                }
+                return result/_notes.Count;
+            }
+        }
+
+        private Etudiant() 
+        { 
+            _notes = new List<int>();
+        }
+        private Etudiant(int[] notes)
+        {
+            _notes = new List<int>(notes);
+        }
+
+        public Etudiant(string nom, string prenom) : this() 
+        {
+            Nom = nom;
+            Prenom = prenom;
+        }
+
+        public Etudiant(string nom, string prenom, int[] notes) : this(notes)
+        {
+            Nom = nom;
+            Prenom = prenom;
+        }
+        //Un constructeur peut utiliser, tout comme les méthodes, des mots-clé à ses paramètres
+        //public Etudiant(string nom, string prenom, params int[] notes) : this(notes)
+        //{
+        //    Nom = nom;
+        //    Prenom = prenom;
+        //}
+    }
+}
---
+++ Demo-Constructeur/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Demo_Constructeur
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Etudiant e1 = new Etudiant("Morre","Thierry");
+            //Etudiant e2 = new Etudiant("Legrain","Samuel", 12, 11, 0, 3, 5 ); //Avec l'utilisation du mot-clé params
+            Etudiant e2 = new Etudiant("Legrain","Samuel", new int[] { 12, 11, 0, 3, 5 } );
+            e2.Notes[2]=100;
+            Console.WriteLine($"La moyenne de {e2.Nom} {e2.Prenom} est de {e2.Moyenne}.");
+        }
+    }
+}
